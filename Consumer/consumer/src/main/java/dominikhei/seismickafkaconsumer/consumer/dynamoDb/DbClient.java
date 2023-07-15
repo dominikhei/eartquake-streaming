87e89353370fa97e6e7ba1458bc77e6f32de487e
Dominik Heilbock <dheilbock@gmx.net>
2023-07-15 18:55:53 +0200

    First draft of the DynamoDb functionality

---
+++ Consumer/consumer/src/main/java/dominikhei/seismickafkaconsumer/consumer/dynamoDb/DbClient.java
@@ -0,0 +1,26 @@
+package dominikhei.seismickafkaconsumer.consumer.dynamoDb;
+
+import org.json.JSONObject;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
+
+public class DbClient {
+
+    public DynamoDB clientBuilder() {
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+        DynamoDB dynamoDB = new DynamoDB(client);
+        return dynamoDB;
+    }
+
+    public void uploadToTable(String tableName, JSONObject json) {
+        DynamoDB dynamoDb = clientBuilder();
+        Table table = dynamoDb.getTable(tableName);
+        String jsonString = json.toString();
+        Item item = new Item().withPrimaryKey("id", 1).withJSON("data", jsonString);
+
+    }
+    
+}
