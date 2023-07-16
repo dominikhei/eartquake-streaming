7ce5c9a26b4d3284b2c78a6d1d049397a804357b
Dominik Heilbock <dheilbock@gmx.net>
2023-07-16 16:34:25 +0200

    Added the functinality to write events to dynamoDb using an assumed role of an Ec2 instance

--- Consumer/consumer/src/main/java/dominikhei/seismickafkaconsumer/consumer/dynamoDb/DbClient.java
+++ Consumer/consumer/src/main/java/dominikhei/seismickafkaconsumer/consumer/dynamoDb/DbClient.java
@@ -2,6 +2,9 @@ package dominikhei.seismickafkaconsumer.consumer.dynamoDb;
 
 import org.json.JSONObject;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.Item;
@@ -10,7 +13,9 @@ import com.amazonaws.services.dynamodbv2.document.Table;
 public class DbClient {
 
     public DynamoDB clientBuilder() {
-        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+            .withRegion(Regions.EU_CENTRAL_1)
+            .build();
         DynamoDB dynamoDB = new DynamoDB(client);
         return dynamoDB;
     }
@@ -21,6 +26,6 @@ public class DbClient {
         String id = json.get("uuid").toString();
         String jsonString = json.toString();
         Item item = new Item().withPrimaryKey("id", id).withJSON("data", jsonString);
-    }
-    
+        table.putItem(item);
+    } 
 }
