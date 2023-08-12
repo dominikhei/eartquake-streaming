a3c1ecd224f7ac7caa8bc6afb35b7db9ceef2f05
Dominik Heilbock <dheilbock@gmx.net>
2023-08-12 13:56:58 +0200

    removed unnecessary commands in initiate.sh

--- terraform/initiate.sh
+++ terraform/initiate.sh
@@ -7,10 +7,6 @@ sudo chmod 666 /var/run/docker.sock
 
 sudo apt-get -y install git
 
-docker plugin install grafana/loki-docker-driver:latest --alias loki --grant-all-permissions
-
-sudo systemctl restart docker
-
 git clone https://github.com/dominikhei/eartquake-streaming.git
 
 cd eartquake-streaming/streaming
